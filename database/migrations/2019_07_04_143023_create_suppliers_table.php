7fbdd9fe774b5b26d241ecac2f6a0b838fabae85
Ezra Akeba <akebaezra@gmail.com>
2019-07-04 17:37:05 +0300

    Create suppliers migration

---
+++ database/migrations/2019_07_04_143023_create_suppliers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id', false, true);
+            $table->integer('tender_id', false, true);
+            $table->string('address');
+            $table->json('details');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')
+                ->on('users');
+            $table->foreign('tender_id')->references('id')
+                ->on('tenders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
