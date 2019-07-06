42c123366ea488e423a5e763013b3ece7b44a7ff
Ezra Akeba <akebaezra@gmail.com>
2019-07-06 17:50:09 +0300

    Create supplier model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    protected $casts = [
+        'details' => 'array',
+        'is_deleted' => 'boolean'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function tender()
+    {
+        return $this->belongsTo('App\Models\Tender');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
+    public function payments()
+    {
+        return $this->hasManyThrough('App\Models\Payment', 'App\Models\Order');
+    }
+}
