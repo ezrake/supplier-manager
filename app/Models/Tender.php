59d5df8bf7eef53b076895d87f5f788b438dacf7
Ezra Akeba <akebaezra@gmail.com>
2019-07-06 17:23:24 +0300

    Create tender model

---
+++ app/Models/Tender.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tender extends Model
+{
+
+    protected $casts = [
+        'details' => 'array',
+        'is_deleted' => 'boolean'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'expiry'
+    ];
+
+    public function requests()
+    {
+        return $this->hasMany('App\Models\Request');
+    }
+
+    public function supplier()
+    {
+        return $this->hasOne('App\Models\Supplier');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
+    public function payments()
+    {
+        return $this->hasManyThrough('App\Models\Payment', 'App\Models\Orders');
+    }
+}
