c7dfa4f32dd6e7615d88a73a5e0bca3c96821e88
Ezra Akeba <akebaezra@gmail.com>
2019-07-31 14:28:24 +0300

    Create order factory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Order;
+use Faker\Generator as Faker;
+
+$factory->define(Order::class, function (Faker $faker) {
+    $details = [];
+    //to represent an order having different items of same type e.g. stationery
+    //will be overwritten to summary of corresponding requisitions
+    for ($i = 0; $i < 10; ++$i) {
+        array_push($details, [
+            $faker->word => [
+                'amount' => $faker->randomDigit,
+                'description' => $faker->text(),
+            ]
+        ]);
+    }
+
+    return [
+        'details' => json_encode($details),
+        'supplier_id' => function ($faker) {
+            return $faker->randomElement(
+                App\Models\Supplier::all()
+                    ->pluck('id')
+            );
+        },
+        'tender_id' => function (array $order) {
+            return App\Models\Supplier::find($order['supplier_id'])
+                ->tender_id;
+        }
+    ];
+});
+
+$factory->state(Order::class, 'delivered', [
+    'delivered' => true
+]);
+
+$factory->state(Order::class, 'isDeleted', [
+    'is_deleted' => true
+]);
