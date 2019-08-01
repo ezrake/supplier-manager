00375ec57922487ec4043fb9456f75a32a5f78fd
Ezra Akeba <akebaezra@gmail.com>
2019-08-01 14:16:24 +0300

    Create requisitions table seeder

---
+++ database/seeds/RequisitionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RequisitionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\Requisition::class, 200)->states('waiting')->create();
+        factory(App\Models\Requisition::class, 200)->states('approved')->create();
+        factory(App\Models\Requisition::class, 200)->states('rejected')->create();
+        factory(App\Models\Requisition::class, 200)->states('delivered')->create();
+    }
+}
