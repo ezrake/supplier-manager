a0c2de3c73f59b8583bd0d13846316e22f304e31
Ezra Akeba <akebaezra@gmail.com>
2019-08-12 16:51:30 +0300

    Create tender resource

---
+++ app/Http/Resources/Tender.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Tender extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'details' => $this->details,
+            'status' => $this->status,
+            'orders' => OrderResource::collection($this->whenLoaded('orders')),
+            'payments' => PaymentResource::collection($this->whenLoaded('payments')),
+            'supplier' => $this->whenLoaded('supplier'),
+            'expiry' => $this->expiry,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'deletedAt' => $this->deleted_at
+        ];
+    }
+}
