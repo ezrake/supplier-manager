ad780eeeac88af5b0338f769083e59f3cdede7f5
Ezra Akeba <akebaezra@gmail.com>
2019-08-12 16:52:50 +0300

    Create payment resource

---
+++ app/Http/Resources/Payments.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Payments extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'transanctionDetails' => $this->transaction_details,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at
+        ];
+    }
+}
