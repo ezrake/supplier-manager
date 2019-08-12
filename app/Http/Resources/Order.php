eea6ef9add059983daa258a3485a7d2ccb8b0ee0
Ezra Akeba <akebaezra@gmail.com>
2019-08-12 16:52:22 +0300

    Create order resource

---
+++ app/Http/Resources/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Order extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'details' => $this->details,
+            'delivered' => $this->delivered,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'deletedAt' => $this->deleted_at
+        ];
+    }
+}
