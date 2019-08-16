3d08942f0620f15c476405d7af2c23354d69e2e6
Ezra Akeba <akebaezra@gmail.com>
2019-08-16 18:45:56 +0300

    Customize department model and resource for api

--- app/Http/Resources/Department.php
+++ app/Http/Resources/Department.php
@@ -14,9 +14,28 @@ class Department extends JsonResource
      */
     public function toArray($request)
     {
+
         return [
             'id' => $this->id,
-            'name' => $this->name
+            'name' => $this->name,
+            'requisitions' => $this->when(
+                $request->is('department/*/requisitions'),
+                $this->requisitionsTransform($this->request)
+            )
         ];
+    }
+
+    public function requisitionsTransform(Request $request)
+    {
+        $query = $request->query();
+        if (isset($query['status'])) {
+            $requisitions = $this->requisitions()
+                ->where('status', $query['status'])
+                ->paginate(20);
+            return $requisitions;
+        }
+
+        $requisitions = $this->requisitions()->paginate(20);
+        return $requisitions;
     }
 }
