0355c052fc9cc1ecb1ac87845050fe1ac4e318b1
Ezra Akeba <akebaezra@gmail.com>
2019-08-17 17:28:46 +0300

    Create validator for requisitions

---
+++ app/Http/Requests/StoreRequisition.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequisition extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'department_id' => 'required|exists:departments,id',
+            'order_id' => 'sometimes|exists:orders,id',
+            'items' => 'required',
+            'items.*.name' => 'required|alpha|max:255',
+            'items.*.amount' => 'required',
+            'status' => 'required|alpha|in:waiting, approved,rejected, delivered',
+        ];
+    }
+}
