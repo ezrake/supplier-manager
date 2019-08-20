2cc31ad18ebe9a5e1d9d60e52705e84b1ac2622d
Ezra Akeba <akebaezra@gmail.com>
2019-08-20 18:06:56 +0300

    Add fillable attributes to order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'details', 'supplier_id', 'tender_id'
+    ];
+
     protected $casts = [
         'details' => 'array',
         'delivered' => 'boolean',
