dee270b033feb33fa206d813b4b297aadf875391
Ezra Akeba <akebaezra@gmail.com>
2019-08-20 18:52:47 +0300

    Create supplier validator

---
+++ app/Http/Requests/StoreSupplier.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSupplier extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'contacts' => 'required',
+            'contacts.*.telephone' => 'required,',
+            'contacts.*.address' => 'required|alpha',
+            'account' => 'required',
+            'account.type' => 'required|alpha',
+            'account.number' => 'required|numeric',
+            'account.name' => 'required|alpha',
+            'account.expirationDate' => 'required|date|after:tomorrow',
+            'user_id' => 'required|exists:users,id',
+            'tender_id' => 'required|exists:tenders,id',
+        ];
+    }
+}
