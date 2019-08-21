03ecb1b091d44c83e08eab33666172468f2a0aac
Ezra Akeba <akebaezra@gmail.com>
2019-08-21 17:14:29 +0300

    Create validator for payments

---
+++ app/Http/Requests/StorePayments.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePayments extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'order_id' => 'required|exists:orders,id',
+            'amount' => 'required|numeric',
+            'transaction_details.*.bank_account' => 'required|numeric',
+            'transaction_details.*.authorised_by' => 'required|alpha',
+            'transaction_details.*.type' => 'required|alpha',
+        ];
+    }
+}
