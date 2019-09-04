6a7d073e6bb95d9273ef6361976b0594e26d3ea6
Ezra Akeba <akebaezra@gmail.com>
2019-09-04 17:45:34 +0300

    Create belongsTo validator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,5 +31,22 @@ class AppServiceProvider extends ServiceProvider
             $difference = \array_diff($value, $parameters);
             return \count($difference) == 0 ? true : false;
         });
+
+        Validator::extend('belongsTo', function ($attribute, $value, $parameters, $validator) {
+            //get table name eg supplier_id returns suppliers
+            $table = \explode('_', $attribute)[0] . 's';
+
+            try {
+                //get the foreign key of child table
+                $id = \Illuminate\Support\Facades\DB::table($table)
+                    ->select($parameters[0])
+                    ->where('id', $value)
+                    ->first()->$parameters[0];
+            } catch (\Exception $e) {
+                return false;
+            }
+            //compare the given foreign key with that of child table
+            return $id === $validator->getValue($parameters[0]) ? true : false;
+        });
     }
 }
