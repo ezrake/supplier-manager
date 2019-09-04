c593a2c2556e3649a7c43a14d4000b0986d43923
Ezra Akeba <akebaezra@gmail.com>
2019-09-04 17:44:20 +0300

    Create fieldsIn validator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('fieldsIn', function ($attribute, $value, $parameters, $validator) {
+            //check if the string has ',' and split it into array using the ','
+            $value = explode(',', $value);
+            //find if all values in $value exist in $parameters
+            $difference = \array_diff($value, $parameters);
+            return \count($difference) == 0 ? true : false;
+        });
     }
 }
